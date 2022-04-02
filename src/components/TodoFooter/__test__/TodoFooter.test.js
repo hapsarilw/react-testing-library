d1c2d7d9d023f18575fe013b4c0b071a3c815df1
Hapsari Laksmi Wijayanti <hapsari.laksmiw@gmail.com>
2022-04-02 16:08:19 +0800

    add describe block

--- src/components/TodoFooter/__test__/TodoFooter.test.js
+++ src/components/TodoFooter/__test__/TodoFooter.test.js
@@ -10,50 +10,56 @@ const MockTodoFooter = ({ numberOfIncompleteTasks }) => {
   );
 };
 
-it("p element should be truthy when the number of incomplete tasks is one", () => {
-  render(<MockTodoFooter numberOfIncompleteTasks={5} />);
-  const pElement = screen.getByText(/5 tasks left/i);
-  expect(pElement).toBeInTheDocument();
+describe("TodoFooter", () => {
+  describe("functionality 1", () => {
+    it("should render the correct amount if incomplete tasks", async () => {
+      render(<MockTodoFooter numberOfIncompleteTasks={5} />);
+      const pElement = screen.getByText(/5 tasks left/i);
+      expect(pElement).toBeInTheDocument();
+    });
+  });
+
+  describe("functionality 2", () => {
+    it("should render 'task' when the number of incomplete tasks is one", async () => {
+      render(<MockTodoFooter numberOfIncompleteTasks={1} />);
+      const pElement = screen.getByText(/1 task left/i);
+      expect(pElement).toBeInTheDocument();
+    });
+  });
 });
 
-it("p element should be truthy when the number of incomplete tasks is one", () => {
-  render(<MockTodoFooter numberOfIncompleteTasks={1} />);
-  const pElement = screen.getByText(/1 task left/i);
-  expect(pElement).toBeInTheDocument();
-});
+// it("p element should be truthy when the number of incomplete tasks is one", () => {
+//   render(<MockTodoFooter numberOfIncompleteTasks={1} />);
+//   const pElement = screen.getByText(/1 task left/i);
+//   expect(pElement).toBeTruthy();
+// });
 
-it("p element should be truthy when the number of incomplete tasks is one", () => {
-  render(<MockTodoFooter numberOfIncompleteTasks={1} />);
-  const pElement = screen.getByText(/1 task left/i);
-  expect(pElement).toBeTruthy();
-});
+// it('"task" should be visible when the number of incomplete tasks is one', () => {
+//   render(<MockTodoFooter numberOfIncompleteTasks={1} />);
+//   const pElement = screen.getByText(/1 task left/i);
+//   expect(pElement).toBeVisible();
+// });
 
-it('"task" should be visible when the number of incomplete tasks is one', () => {
-  render(<MockTodoFooter numberOfIncompleteTasks={1} />);
-  const pElement = screen.getByText(/1 task left/i);
-  expect(pElement).toBeVisible();
-});
+// it("should contain p tag with correct text", () => {
+//   render(<MockTodoFooter numberOfIncompleteTasks={1} />);
+//   const pElement = screen.getByText(/1 task left/i);
+//   expect(pElement).toContainHTML("p");
+// });
 
-it("should contain p tag with correct text", () => {
-  render(<MockTodoFooter numberOfIncompleteTasks={1} />);
-  const pElement = screen.getByText(/1 task left/i);
-  expect(pElement).toContainHTML("p");
-});
+// it("should render correct text content", () => {
+//   render(<MockTodoFooter numberOfIncompleteTasks={1} />);
+//   const pElement = screen.getByText(/1 task left/i);
+//   expect(pElement).toHaveTextContent("1 task left");
+// });
 
-it("should render correct text content", () => {
-  render(<MockTodoFooter numberOfIncompleteTasks={1} />);
-  const pElement = screen.getByText(/1 task left/i);
-  expect(pElement).toHaveTextContent("1 task left");
-});
+// it("should render correct text content", () => {
+//   render(<MockTodoFooter numberOfIncompleteTasks={1} />);
+//   const pElement = screen.getByText(/1 task left/i);
+//   expect(pElement).not.toBeFalsy();
+// });
 
-it("should render correct text content", () => {
-  render(<MockTodoFooter numberOfIncompleteTasks={1} />);
-  const pElement = screen.getByText(/1 task left/i);
-  expect(pElement).not.toBeFalsy();
-});
-
-it("should render correct text content", () => {
-  render(<MockTodoFooter numberOfIncompleteTasks={1} />);
-  const pElement = screen.getByTestId("para");
-  expect(pElement.textContent).toBe("1 task left");
-});
+// it("should render correct text content", () => {
+//   render(<MockTodoFooter numberOfIncompleteTasks={1} />);
+//   const pElement = screen.getByTestId("para");
+//   expect(pElement.textContent).toBe("1 task left");
+// });
